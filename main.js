e8ae7765668520466c81b986424a5721fc3ea27e
LenaDishchenko <jasidua@gmail.com>
2020-03-16 18:57:11 +0200

    variables

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert( 'Привет, мир2222!' );
+"use strict";
+
+alert( 'Я JavaScript!' );
+
+[1, 2].forEach(alert);
+
+let message  = 'Hello1!';
+alert(message);
+
+let admin;
+let name;
+name = "Джон";
+admin = name;
+aleert(admin);
+
+let ourPlanetName = "Земля";
+let currentUserName = "Alex";
+
