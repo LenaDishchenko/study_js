9cbbba68692484be731e23ea6e07f2ffb80b3ba7
LenaDishchenko <jasidua@gmail.com>
2020-03-25 19:57:10 +0200

    ToDo list - add items quantity output - checked item state now also updated in localStorage. Save this state after page reload - Styles for todo list page

--- main.js
+++ main.js
@@ -2,26 +2,38 @@
 
 window.onload = function () {
 
+    //date data output
     let today = new Date();
     let formatDate = today.toDateString();
     let selectElement = document.getElementById('date');
     selectElement.innerHTML = formatDate;
-    
+
+    //todoList array creation
     var todoList = [];
+
+    //all items from localStorage output
     if (localStorage.getItem('todo') != this.undefined) {
         todoList = JSON.parse(localStorage.getItem('todo'));
         out();
     }
+
+    // items quantity output
+    let tasksCounterElement = document.getElementById('tasks-counter');
+    tasksCounterElement.innerHTML = todoList.length + ' tasks';
+
+    // add items to the list function
     document.getElementById('js-add-button').onclick = function (){
         var d = document.getElementById('js-add-input').value;
         var temp = {};
         temp.id = Date.now();
         temp.todo = d;
         temp.check = false;
-        // var i = todoList.length;
         todoList.push(temp);
         out();
+        // update localStorage
         localStorage.setItem('todo', JSON.stringify(todoList));
+        // update items quantity
+        tasksCounterElement.innerHTML = todoList.length + ' tasks';
     };
 
     var list = document.querySelector('ul');
@@ -31,38 +43,42 @@ window.onload = function () {
         if (ev.target.tagName === 'LI') {
             ev.target.classList.toggle('checked');
             localStorage.setItem('todo', JSON.stringify(todoList));
-            var date = this.getAttribute('data-id');
-            var div1 = document.getElementsByTagName("LI");
-            var align = div1.getAttribute("data-id");
-            alert(align);
+            var id = ev.target.getAttribute('data-id');
             for (var key in todoList) {
-                if (todoList[key].id == date) {
+                if (todoList[key].id == id) {
+                    console.log('test11111');
                     todoList[key].check = true;
-                    console.log('ura');
+                    // update localStorage
+                    localStorage.setItem('todo', JSON.stringify(todoList));
                 }
             }
+            debugger;
         }
         // delete item
-        if (ev.target.tagName === 'SPAN') {
+        if (ev.target.tagName === 'I') {
             console.log('test3');
             ev.target.parentNode.classList.add("hidden");
         }
     }, false);
 
-    // clear all list
+    // clear all list and localStorage update
     document.getElementById('js-clear-button').onclick = function (){
         localStorage.removeItem('todo');
         document.getElementById('js-todo-list').innerHTML = "";
         todoList = [];
+        // update items quantity
+        tasksCounterElement.innerHTML = todoList.length + ' tasks';
     };
+
+    // new items output function
     function out() {
         var out = '';
         for (var key in todoList) {
             if (todoList[key].check == true) {
-                out += '<li class="checked" data-id="' + todoList[key].id + '">' + todoList[key].todo + '<span class="close">×</span></li>';
+                out += '<li class="checked" data-id="' + todoList[key].id + '">' + todoList[key].todo + '<i class="close fa fa-trash-o" aria-hidden="true"></i>';
             }
             else {
-                out += '<li data-id="' + todoList[key].id + '">' + todoList[key].todo + '<span class="close">×</span></li>';
+                out += '<li data-id="' + todoList[key].id + '">' + todoList[key].todo + '<i class="delete fa fa-trash-o" aria-hidden="true"></i>';
             }
         }
         document.getElementById('js-todo-list').innerHTML = out;
