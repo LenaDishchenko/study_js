25c53458e8e408e52308a5e7fa1d990cb6761ff4
LenaDishchenko <jasidua@gmail.com>
2020-03-31 18:00:16 +0300

    - oportunity to create todo lists

--- main.js
+++ main.js
@@ -10,14 +10,22 @@ window.onload = function () {
 
     //todoList array creation
     var todoList = [];
+    var todoLists = [];
 
     // items quantity output
     let tasksCounterElement = document.getElementById('tasks-counter');
+    let tasksCounterElementDeleted = document.getElementById('tasks-counter-deleted');
 
     //all items from localStorage output
-    if (localStorage.getItem('todo') != this.undefined) {
-        todoList = JSON.parse(localStorage.getItem('todo'));
+    if (localStorage.getItem('todos') != this.undefined) {
+        todoList = JSON.parse(localStorage.getItem('todos'));
         out();
+    }
+
+    //all items from localStorage output
+    if (localStorage.getItem('lists') != this.undefined) {
+        todoLists = JSON.parse(localStorage.getItem('lists'));
+        outMenuItems();
     }
 
     // add items to the list function
@@ -38,9 +46,27 @@ window.onload = function () {
             alert('Input value is empty!');
         }
     };
+    
+    // add new list function
+    document.getElementById('tasks-list-add-button').onclick = function (){
+        var inputValue = document.getElementById('tasks-list-add-input').value;
+        if (inputValue != "") {
+            todoLists.push({
+                id: Date.now(),
+                name: inputValue,
+                list: todoList,
+            });
+        }
+        else {
+            alert('Input value is empty!');
+        }
+        document.getElementById('js-add-input').value = "";
+        document.getElementById('js-add-input').focus();
+        refreshState();
+    };
 
-    var list = document.querySelector('ul');
-    list.addEventListener('click', function(ev) {
+    var activeList = document.getElementById('js-active-todo-list');
+    activeList.addEventListener('click', function(ev) {
         // check item
         var id = ev.target.parentNode.getAttribute('data-id');
         if (ev.target.tagName === 'SPAN') {
@@ -54,28 +80,39 @@ window.onload = function () {
             todoList = todoList.map(toggleChecked);
             refreshState();
         }
-        
         // delete item
         if (ev.target.classList.contains('action-icon')) {
-            var onlyMatchingIds = function (todo) {
-                return todo.id != id;
-            }
-            var toggleDeleted = function (todo) {
+            var deleteItem = function (todo) {
                 if (todo.id == id) {
                     todo.delete = true;
                 }
                 return todo;            
             }
-            todoList = todoList.map(toggleDeleted);
+            todoList = todoList.map(deleteItem);
             refreshState();
-            debugger;
+        }
+    }, false);
+
+    var deletedList = document.getElementById('js-deleted-todo-list');
+    deletedList.addEventListener('click', function(ev) {
+        // restore item
+        var id = ev.target.parentNode.getAttribute('data-id');
+        if (ev.target.classList.contains('action-icon')) {
+            var restoreItem = function (todo) {
+                if (todo.id == id) {
+                    todo.delete = false;
+                }
+                return todo;            
+            }
+            todoList = todoList.map(restoreItem);
+            refreshState();
         }
     }, false);
 
     // clear all list and localStorage update
     document.getElementById('js-clear-button').onclick = function (){
-        localStorage.removeItem('todo');
-        document.getElementById('js-todo-list').innerHTML = "";
+        localStorage.removeItem('lists');
+        document.getElementById('js-active-todo-list').innerHTML = "";
         todoList = [];
         refreshState();
     };
@@ -97,18 +134,41 @@ window.onload = function () {
             deletedOutput += `<li data-id="${deletedTodoList[key].id}"><span ${deletedTodoList[key].check ? "class='checked'" : ""}>${deletedTodoList[key].text}</span><i class="action-icon fa fa-undo" aria-hidden="true"></i>`
         }
         tasksCounterElement.innerHTML = activeTodoList.length + ' tasks';
+        tasksCounterElementDeleted.innerHTML = deletedTodoList.length + ' tasks';
         document.getElementById('js-active-todo-list').innerHTML = actveOutput;
         document.getElementById('js-deleted-todo-list').innerHTML = deletedOutput;
+    };
+    // new items output function
+    function outMenuItems() {
+        var actveOutput = '';
+        for (var key in todoLists) {
+            actveOutput += `<div data-id="${todoLists[key].id}"><span>${todoLists[key].name}</span></div>`
+        }
+        document.getElementById('js-munu').innerHTML = actveOutput;
+        console.log('ef');
     };
 
     // localStorage update
     function localStorageUpdate() {
-        localStorage.setItem('todo', JSON.stringify(todoList));
+        localStorage.setItem('todos', JSON.stringify(todoList));
+        localStorage.setItem('lists', JSON.stringify(todoLists));
     };
 
     // refreshState
     function refreshState() {
         localStorageUpdate();
         out();
+        outMenuItems();
     };
+
+    // menu
+    document.getElementById('tasks-list-deleted-button').onclick = function (){
+        document.getElementById('js-deleted-todo-list').classList.remove('hidden');
+        document.getElementById('js-active-todo-list').classList.add('hidden');
+    };
+    document.getElementById('tasks-list-button').onclick = function (){
+        document.getElementById('js-active-todo-list').classList.remove('hidden');
+        document.getElementById('js-deleted-todo-list').classList.add('hidden');
+    };
+
 };
